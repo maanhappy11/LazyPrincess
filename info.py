7447c499a62c3dc700f3723dd2042280c6cf5ff7
maanhappy11 <108676319+maanhappy11@users.noreply.github.com>
2024-06-27 08:55:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://noname5911315:maanjatt@yuvrajmaan.ypfzoc6.mongodb.net/?retryWrites=true&w=majority&appName=yuvrajmaan")
 DATABASE_NAME = environ.get('DATABASE_NAME', "yuvrajmaan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
